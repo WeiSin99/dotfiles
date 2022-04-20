167c36c7617ec59af10c1d9b7b652c34d96fab5b
WeiSin99 <looweisin008@gmail.com>
2022-04-20 18:28:28 +0800

    update vim script

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
+
+mkdir -p ~/.config/nvim/plugin
+mkdir -p ~/.config/nvim/ftplugin
+
 for f in `find . \( -iname \*.lua -o -iname \*.vim \)`; do
     rm -rf ~/.config/nvim/$f
     ln -s ~/Desktop/dotfiles/$f ~/.config/nvim/$f
