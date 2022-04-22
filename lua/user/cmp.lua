47da3a8fba2962c07fa8976559cbe3c1c49c6fc5
WeiSin99 <looweisin008@gmail.com>
2022-04-23 02:20:14 +0800

    remove snippet

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -4,14 +4,6 @@ if not cmp_status_ok then
   return
 end
 
-local snip_status_ok, luasnip = pcall(require, "luasnip")
-if not snip_status_ok then
-  vim.notify("luasnip not found")
-  return
-end
-
-require("luasnip/loaders/from_vscode").lazy_load()
-
 local kind_icons = {
   Text = "",
   Method = "m",
@@ -41,11 +33,6 @@ local kind_icons = {
 }
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
   mapping = {
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -75,7 +62,6 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
-        luasnip = "[Snippet]",
         buffer = "[Buffer]",
       })[entry.source.name]
       return vim_item
@@ -84,7 +70,6 @@ cmp.setup {
   sources = {
     { name = "nvim_lsp" },
     { name = "buffer" },
-    { name = "luasnip" },
   },
   window = {
     completion = cmp.config.window.bordered(),
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -31,12 +31,7 @@ return packer.startup(function(use)
   -- autocompletion
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-buffer"
-  use "saadparwaiz1/cmp_luasnip"
   use "hrsh7th/cmp-nvim-lsp"
-
-  -- snippet
-  use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets"
 
   -- LSP
   use "neovim/nvim-lspconfig"
