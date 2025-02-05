08abf7802d3e1681d7980bc614abc21d9ecb4084
WeiSin99 <looweisin008@gmail.com>
2025-02-05 23:06:59 +0000

    apply formatting

--- nvim/lua/weisin/plugins/linting.lua
+++ nvim/lua/weisin/plugins/linting.lua
@@ -1,21 +1,21 @@
 return {
-  "mfussenegger/nvim-lint",
+  'mfussenegger/nvim-lint',
   event = {
-    "BufReadPre",
-    "BufNewFile",
+    'BufReadPre',
+    'BufNewFile',
   },
   config = function()
-    local lint = require("lint")
+    local lint = require('lint')
 
     lint.linters_by_ft = {
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
+      javascript = { 'eslint' },
+      typescript = { 'eslint' },
+      javascriptreact = { 'eslint' },
+      typescriptreact = { 'eslint' },
     }
 
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
+      group = vim.api.nvim_create_augroup('nvim-lint', { clear = true }),
       callback = function()
         lint.try_lint()
       end,
--- nvim/lua/weisin/plugins/lsp/lspconfig.lua
+++ nvim/lua/weisin/plugins/lsp/lspconfig.lua
@@ -1,56 +1,56 @@
 return {
-  "neovim/nvim-lspconfig",
+  'neovim/nvim-lspconfig',
   dependencies = {
-    { "williamboman/mason.nvim", opts = {} },
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "saghen/blink.cmp",
+    { 'williamboman/mason.nvim', opts = {} },
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'saghen/blink.cmp',
   },
   config = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
       callback = function(event)
         local map = function(keys, func, desc, mode)
-          mode = mode or "n"
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map("gd", "<cmd>FzfLua lsp_definitions<cr>", "[G]oto [D]efinition")
+        map('gd', '<cmd>FzfLua lsp_definitions<cr>', '[G]oto [D]efinition')
 
         -- Find references for the word under your cursor.
-        map("gr", "<cmd>FzfLua lsp_references<cr>", "[G]oto [R]eferences")
+        map('gr', '<cmd>FzfLua lsp_references<cr>', '[G]oto [R]eferences')
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map("gI", "<cmd>FzfLua lsp_implementations<cr>", "[G]oto [I]mplementation")
+        map('gI', '<cmd>FzfLua lsp_implementations<cr>', '[G]oto [I]mplementation')
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map("<leader>D", "<cmd>FzfLua lsp_typedefs<cr>", "Type [D]efinition")
+        map('<leader>D', '<cmd>FzfLua lsp_typedefs<cr>', 'Type [D]efinition')
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map("<leader>ds", "<cmd>FzfLua lsp_document_symbols<cr>", "[D]ocument [S]ymbols")
+        map('<leader>ds', '<cmd>FzfLua lsp_document_symbols<cr>', '[D]ocument [S]ymbols')
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map("<leader>ws", "<cmd>FzfLua lsp_workspace_symbols<cr>", "[W]orkspace [S]ymbols")
+        map('<leader>ws', '<cmd>FzfLua lsp_workspace_symbols<cr>', '[W]orkspace [S]ymbols')
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
-        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
@@ -59,24 +59,24 @@ return {
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-          local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
             callback = vim.lsp.buf.document_highlight,
           })
 
-          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
             buffer = event.buf,
             group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
           })
 
-          vim.api.nvim_create_autocmd("LspDetach", {
-            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
             callback = function(event2)
               vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+              vim.api.nvim_clear_autocmds({ group = 'kickstart-lsp-highlight', buffer = event2.buf })
             end,
           })
         end
@@ -88,15 +88,15 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
 
     local servers = {
       lua_ls = {
         settings = {
           Lua = {
             diagnostics = {
-              globals = { "vim" },
-              disable = { "missing-fields" },
+              globals = { 'vim' },
+              disable = { 'missing-fields' },
             },
           },
         },
@@ -105,12 +105,12 @@ return {
         -- explicitly add default filetypes, so that we can extend
         -- them in related extras
         filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx",
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
         },
         settings = {
           complete_function_calls = true,
@@ -125,14 +125,14 @@ return {
             },
           },
           typescript = {
-            updateImportsOnFileMove = { enabled = "always" },
+            updateImportsOnFileMove = { enabled = 'always' },
             suggest = {
               completeFunctionCalls = true,
             },
             inlayHints = {
               enumMemberValues = { enabled = true },
               functionLikeReturnTypes = { enabled = true },
-              parameterNames = { enabled = "literals" },
+              parameterNames = { enabled = 'literals' },
               parameterTypes = { enabled = true },
               propertyDeclarationTypes = { enabled = true },
               variableTypes = { enabled = false },
@@ -143,12 +143,12 @@ return {
     }
 
     local formatters = {
-      "stylua",
-      "prettier",
+      'stylua',
+      'prettier',
     }
 
     local linters = {
-      "eslint",
+      'eslint',
     }
 
     -- convert keys in servers into an array as an list of lsp to install
@@ -156,9 +156,9 @@ return {
     vim.list_extend(ensure_installed, formatters)
     vim.list_extend(ensure_installed, linters)
 
-    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
-    require("mason-lspconfig").setup({
+    require('mason-lspconfig').setup({
       automatic_installation = false,
       handlers = {
         function(server_name)
@@ -169,8 +169,8 @@ return {
 
           local server = servers[server_name] or {}
 
-          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-          require("lspconfig")[server_name].setup(server)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
         end,
       },
     })
