bf7d3226a9a3b527da348ad2f212d1c27349d9ef
WeiSin99 <looweisin008@gmail.com>
2025-02-16 17:47:42 +0000

    Obsidian flow refinements
    Obsidian flow refinements

--- nvim/lua/weisin/plugins/telescope.lua
+++ nvim/lua/weisin/plugins/telescope.lua
@@ -21,8 +21,18 @@ return {
     { 'nvim-telescope/telescope-ui-select.nvim' },
   },
   config = function()
+    local actions = require('telescope.actions')
+
     require('telescope').setup({
       defaults = {
+        mappings = {
+          i = {
+            -- map actions.which_key to <C-h> (default: <C-/>)
+            -- actions.which_key shows the mappings for your picker,
+            -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+            ['<Esc>'] = actions.close,
+          },
+        },
         layout_config = {
           prompt_position = 'top',
         },
