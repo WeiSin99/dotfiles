9fc22c9c391b25c3dabf73a176f74d4f879c6476
Wei Sin Loo <45273464+WeiSin99@users.noreply.github.com>
2025-02-27 23:16:10 +0000

    update installation script (#28)

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
-ln -s /Users/weisin/software-engineering/projects/dotfiles-public/nvim ~/.config/nvim
-ln -s /Users/weisin/software-engineering/projects/dotfiles-public/scripts/tmux-sessionizer ~/.local/bin
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+ln -s "$SCRIPT_DIR/nvim" ~/.config/nvim
+ln -s "$SCRIPT_DIR/scripts/tmux-sessionizer" ~/.local/bin
