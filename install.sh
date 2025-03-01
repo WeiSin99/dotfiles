ec77d5ca516afce4dd00444f9274613114b6ba32
WeiSin99 <looweisin008@gmail.com>
2025-03-01 13:08:55 +0000

    bat configurations

--- install.sh
+++ install.sh
@@ -2,5 +2,14 @@
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+rm ~/.config/nvim
 ln -s "$SCRIPT_DIR/nvim" ~/.config/nvim
-ln -s "$SCRIPT_DIR/scripts/tmux-sessionizer" ~/.local/bin
+
+rm ~/.config/bat
+ln -s "$SCRIPT_DIR/bat" ~/.config/bat
+
+rm ~/.local/bin/tmux-sessionizer
+ln -s "$SCRIPT_DIR/scripts/tmux-sessionizer" ~/.local/bin/tmux-sessionizer
+
+rm ~/.config/lazygit/config.yml
+ln -s "$SCRIPT_DIR/lazygit/config.yml" ~/.config/lazygit/config.yml
