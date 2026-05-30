f9a4f51fcac1af503f261a1c39e1ecec9d1979d0
Wei Sin Loo <45273464+WeiSin99@users.noreply.github.com>
2026-05-30 23:56:37 +0100

    ghostty config and several updates (#45)

--- nvim/lua/weisin/plugins/obsidian.lua
+++ nvim/lua/weisin/plugins/obsidian.lua
@@ -8,52 +8,25 @@ return {
     'BufNewFile ' .. vim.fn.expand('~') .. '/Library/Mobile Documents/iCloud~md~obsidian/Documents/ws-vault/*.md',
   },
   opts = {
-    legacy_commands = false,
-    ui = { enable = false },
     workspaces = {
       {
         name = 'My vault',
         path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ws-vault',
       },
     },
-    notes_subdir = '00 - Inbox',
+    legacy_commands = false,
+    ui = { enable = false },
+    frontmatter = { enabled = false },
+    notes_subdir = '00-Inbox',
     new_notes_location = 'notes_subdir',
+    templates = {
+      folder = '99-meta/templates',
+    },
     daily_notes = {
-      folder = '01 - Daily Notes',
+      folder = '70-journal',
       date_format = '%Y-%m-%d',
       alias_format = nil,
-      default_tags = { 'daily-notes' },
-      template = 'daily-note.md',
-    },
-    templates = {
-      folder = 'templates',
-    },
-    frontmatter = {
-      sort = { 'id', 'title', 'aliases', 'topics', 'related', 'tags', 'last_updated', 'created_at' },
-      func = function(note)
-        local now = os.date('%Y-%m-%d %H:%M:%S') --[[@as string]]
-
-        local aliases = vim.tbl_filter(function(alias)
-          return alias ~= note.id and alias ~= note.title and alias ~= note.path.stem
-        end, note.aliases)
-        table.insert(aliases, note.path.stem)
-
-        local out = { aliases = aliases, topics = {}, related = {}, tags = note.tags, created_at = now }
-
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-
-        out.id = out.created_at:gsub('[^%w]', '')
-        out.title = note.path.stem
-        out.last_updated = now
-
-        return out
-      end,
+      template = 'daily.md',
     },
     note_id_func = function(title)
       if title ~= nil then
